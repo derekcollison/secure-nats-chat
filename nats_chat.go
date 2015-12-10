bee2128cdf380ff4b61c260a4833d800fdf0a562
Derek Collison <derek.collison@gmail.com>
2015-12-09 19:24:33 -0800

    use name as data, cleanup

--- nats_chat.go
+++ nats_chat.go
@@ -33,7 +33,12 @@ var keyHash []byte
 // Cipher
 var gcm cipher.AEAD
 var nonce []byte
-var block cipher.Block
+
+// Messages we send
+type chat struct {
+	Name string
+	Msg  []byte
+}
 
 func main() {
 	log.SetFlags(0)
@@ -51,7 +56,7 @@ func main() {
 
 	// Create cipher
 	var err error
-	block, err = aes.NewCipher(keyHash)
+	block, err := aes.NewCipher(keyHash)
 	if err != nil {
 		log.Fatalf("Can't create cipher: %v\n", err)
 	}
@@ -73,12 +78,6 @@ func main() {
 	log.Printf("Securely connected to %s", natsUrl)
 	ec, _ := nats.NewEncodedConn(nc, nats.GOB_ENCODER)
 
-	// Messages we send
-	type natsChat struct {
-		Name string
-		Msg  []byte
-	}
-
 	// Setup signal handlers to signal leaving.
 	c := make(chan os.Signal, 1)
 	signal.Notify(c, os.Interrupt, os.Kill)
@@ -86,7 +85,7 @@ func main() {
 		for range c {
 			fmt.Printf("\n\n")
 			if name != "" {
-				exit := &natsChat{Name: name, Msg: encrypt("<left>\n")}
+				exit := &chat{Name: name, Msg: encrypt("<left>\n")}
 				ec.Publish(subj, exit)
 				ec.Flush()
 			}
@@ -98,26 +97,27 @@ func main() {
 	fmt.Printf("Enter Name: ")
 	fmt.Scanln(&name)
 
+	// Create a reader for stdin
+	reader := bufio.NewReader(os.Stdin)
+
 	// Subscribe to messages
-	ec.Subscribe(subj, func(msg *natsChat) {
+	ec.Subscribe(subj, func(msg *chat) {
 		if msg.Name == name {
 			return
 		}
-		//		fmt.Printf("Received a message! %+v\n", msg)
-		fmt.Printf("\r[%s] %s", msg.Name, decrypt(msg.Msg))
+		fmt.Printf("\033[2K\r[%s] %s", msg.Name, decrypt(msg))
 		fmt.Printf("[%s] ", name)
 	})
 
 	// Send welcome
-	welcome := &natsChat{Name: name, Msg: encrypt("<joined>\n")}
+	welcome := &chat{Name: name, Msg: encrypt("<joined>\n")}
 	ec.Publish(subj, welcome)
 
 	// Wait on new messages to send
-	reader := bufio.NewReader(os.Stdin)
 	for {
 		fmt.Printf("[%s] ", name)
 		msg, _ := reader.ReadString('\n')
-		ec.Publish(subj, natsChat{Name: name, Msg: encrypt(msg)})
+		ec.Publish(subj, chat{Name: name, Msg: encrypt(msg)})
 	}
 
 	runtime.Goexit()
@@ -125,11 +125,11 @@ func main() {
 
 func encrypt(msg string) []byte {
 	plaintext := []byte(msg)
-	return gcm.Seal(nil, nonce, plaintext, nil)
+	return gcm.Seal(nil, nonce, plaintext, []byte(name))
 }
 
-func decrypt(ciphertext []byte) string {
-	v, err := gcm.Open(nil, nonce, ciphertext, nil)
+func decrypt(msg *chat) string {
+	v, err := gcm.Open(nil, nonce, msg.Msg, []byte(msg.Name))
 	if err != nil {
 		return "<unable to decrypt, wrong key?>\n"
 	}
